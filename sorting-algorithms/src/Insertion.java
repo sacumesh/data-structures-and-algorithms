c88ce0ce267b0a5c265e2d4d79b579c6cdfecacf
Sachiththa Bandaranayake <sacume3@gmail.com>
2023-08-30 18:20:07 +0200

    add loop invariant assertion and implement simple main function to test

--- sorting-algorithms/src/Insertion.java
+++ sorting-algorithms/src/Insertion.java
@@ -2,9 +2,20 @@ import java.util.Arrays;
 
 public class Insertion {
 
+    public static void main(String[] args) {
+        Integer[] a  = {1, 23, 4, 0, 5, 2};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println(isSorted(a));
+    }
+
     public static void sort(Comparable a[]) {
         int n = a.length;
         for (int i = 1; i < n; ++i) {
+
+            // loop invariant a[0: i - 1] is sorted;
+            assert isSorted(a, 0, i - 1);
+
             int j = i - 1;
             while (j >= 0 && less(a[j + 1], a[j])){
                 swap(a, j + 1, j);
@@ -15,9 +26,12 @@ public class Insertion {
     }
 
     public static boolean isSorted(Comparable a[]) {
-        for (int i = 1; i < a.length; ++i)
-            if (less(a[i], a[i - 1]))
-                return false;
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    public static boolean isSorted(Comparable a[], int left, int right) {
+        for (int i = left + 1; i <= right; i ++)
+            if (less(a[i], a[i - 1])) return false;
 
         return true;
     }
